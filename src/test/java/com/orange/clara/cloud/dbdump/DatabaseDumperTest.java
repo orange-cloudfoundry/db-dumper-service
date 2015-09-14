9ec834219207f299180f884dd7df7b47f3911d88
Arthur Halet <arthur.halet@orange.com>
2015-09-14 19:22:16 +0200

    Add tests

---
+++ src/test/java/com/orange/clara/cloud/dbdump/DatabaseDumperTest.java
@@ -0,0 +1,28 @@
+package com.orange.clara.cloud.dbdump;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Copyright (C) 2015 Arthur Halet
+ * <p/>
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE' in this package distribution
+ * or at 'http://opensource.org/licenses/MIT'.
+ * <p/>
+ * Author: Arthur Halet
+ * Date: 14/09/2015
+ */
+public class DatabaseDumperTest {
+
+    @Test
+    public void testGetDumpCommandLine() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRestoreCommandLine() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/orange/clara/cloud/dbdump/action/AbstractDbActionTest.java
@@ -0,0 +1,21 @@
+package com.orange.clara.cloud.dbdump.action;
+
+import static org.junit.Assert.*;
+
+/**
+ * Copyright (C) 2015 Arthur Halet
+ * <p/>
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE' in this package distribution
+ * or at 'http://opensource.org/licenses/MIT'.
+ * <p/>
+ * Author: Arthur Halet
+ * Date: 14/09/2015
+ */
+public class AbstractDbActionTest {
+
+    @org.junit.Test
+    public void testRunCommandLine() throws Exception {
+
+    }
+}
