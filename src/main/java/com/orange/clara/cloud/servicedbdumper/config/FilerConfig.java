40cade0d0f18eda0bccbed566f1b9eb8a8afc1c0
ArthurHlt <arthurh.halet@gmail.com>
2016-02-26 12:44:37 +0100

    fix acceptance test sripts

---
+++ src/main/java/com/orange/clara/cloud/servicedbdumper/config/FilerConfig.java
@@ -0,0 +1,14 @@
+package com.orange.clara.cloud.servicedbdumper.config;
+
+/**
+ * Copyright (C) 2016 Orange
+ * <p>
+ * This software is distributed under the terms and conditions of the 'Apache-2.0'
+ * license which can be found in the file 'LICENSE' in this package distribution
+ * or at 'https://opensource.org/licenses/Apache-2.0'.
+ * <p>
+ * Author: Arthur Halet
+ * Date: 26/02/2016
+ */
+public class FilerConfig {
+}
