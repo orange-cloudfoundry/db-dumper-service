af6068dbd1d4c7bcf02fdd446205a609f7cad9a5
ArthurHlt <arthurh.halet@gmail.com>
2016-04-30 04:31:41 +0200

    [concourse] make test faster

--- pipelines/ci/integration-task.sh
+++ pipelines/ci/integration-task.sh
@@ -4,5 +4,5 @@ set -e
 cd db-dumper-service
 
 bin/install-binaries
-
+export MAVEN_OPTS="${MAVEN_OPTS} -Djava.security.egd=file:/dev/./urandom"
 mvn clean integration-test failsafe:verify
--- pipelines/ci/unit-task.sh
+++ pipelines/ci/unit-task.sh
@@ -20,4 +20,6 @@ echo "" > src/main/resources/binaries/redis/bin/rutil
 
 chmod -R +x src/main/resources/binaries
 
+export MAVEN_OPTS="${MAVEN_OPTS} -Djava.security.egd=file:/dev/./urandom"
+
 mvn test
