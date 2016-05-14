acf720b6d3eb629c44e58205ca9a27bcb1a4486a
ArthurHlt <arthurh.halet@gmail.com>
2016-05-14 12:25:09 +0200

    choose polonia's mirror for downloading mariadb binaries in test

--- pipelines/ci/acceptance-task.sh
+++ pipelines/ci/acceptance-task.sh
@@ -2,7 +2,9 @@
 
 set -e
 cd db-dumper-service
+
+sed -i "" 's|http://nyc2\.mirrors\.digitalocean\.com/mariadb|http://mariadb\.kisiek\.net|' bin/install-binaries
 bin/install-binaries
-export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:///dev/urandom"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:///dev/./urandom"
 
 mvn test -q -Dtest.groups=acceptance-tests
